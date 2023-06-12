5e544c3b79769b6381bfff98654ca4ded260a5d8
Adam Kadar <adam.kadar@digitalthinkers.com>
2023-06-12 12:53:01 +0200

    feat: add login route

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -24,4 +24,32 @@ class AuthController extends Controller
 
         return response()->json($user);
     }
+
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required|min:6'
+        ]);
+
+        $credentials = request(['email', 'password']);
+
+        if (!auth()->attempt($credentials)) {
+            return response()->json([
+                'message' => 'The given data was invalid.',
+                'errors' => [
+                    'password' => [
+                        'Invalid credentials'
+                    ],
+                ]
+            ], 422);
+        }
+    
+        $user = User::where('email', $request->email)->first();
+        $authToken = $user->createToken('auth-token')->plainTextToken;
+    
+        return response()->json([
+            'access_token' => $authToken,
+        ]);
+    }
 }
