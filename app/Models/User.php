a748e2c5bc05e638b6a76b853382cd9f3f918145
Adam Kadar <adam.kadar@digitalthinkers.com>
2023-06-13 12:22:19 +0200

    feat: add role guard

--- app/Models/User.php
+++ app/Models/User.php
@@ -14,6 +14,11 @@ class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
+    public function hasRole(string $role)
+    {
+        return $this->role === $role;
+    }
+
     /**
      * The attributes that are mass assignable.
      *
--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,7 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+
 Route::post('register', 'App\\Http\\Controllers\\Api\\AuthController@register');
 Route::post('login', 'App\\Http\\Controllers\\Api\\AuthController@login');
 
@@ -20,7 +21,7 @@ Route::group(['namespace' => 'App\Http\Controllers\Api', 'middleware' => 'auth:s
     Route::get('/user', function (Request $request) {
         return $request->user();
     });
-    
-    Route::apiResource('products/seller', SellerProductController::class);
-    Route::apiResource('products/buyer', BuyerProductController::class);
+
+    Route::apiResource('products/seller', SellerProductController::class)->middleware('role:seller');
+    Route::apiResource('products/buyer', BuyerProductController::class)->middleware('role:buyer');
 });
