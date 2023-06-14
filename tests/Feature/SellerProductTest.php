021046ae7434db61911a6be785387785bb6bd1e6
Adam Kadar <adam.kadar@digitalthinkers.com>
2023-06-14 15:57:33 +0200

    feat: add seller get product filter test

--- tests/Feature/SellerProductTest.php
+++ tests/Feature/SellerProductTest.php
@@ -2,13 +2,11 @@
 
 namespace Tests\Feature;
 
-use App\Http\Resources\SellerProductCollection;
 use App\Http\Resources\SellerProductResource;
 use App\Models\Product;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use App\Models\User;
-use Error;
 use Tests\TestCase;
 use Laravel\Sanctum\Sanctum;
 use Illuminate\Http\Response;
@@ -39,6 +37,9 @@ class SellerProductTest extends TestCase
 
         $this->routes = [
             'index' => '/api/products/seller',
+            'index-filter-by-name' => '/api/products/seller?name[eq]=Cola',
+            'index-filter-by-cost' => '/api/products/seller?cost[gt]=3',
+            'index-filter-by-seller' => '/api/products/seller?sellerId[eq]=-1',
             'store' => '/api/products/seller',
             'show' => '/api/products/seller/1',
             'update' => '/api/products/seller/1',
@@ -48,6 +49,14 @@ class SellerProductTest extends TestCase
         $this->user = User::factory(['role' => User::ROLES['SELLER']])->create();
 
         Sanctum::actingAs($this->user);
+    }
+
+    public function tearDown(): void
+    {
+        // Clear only the data created during the test
+        $this->refreshDatabase();
+
+        parent::tearDown();
     }
 
     public function testASellerCanCreateProduct(): void
@@ -80,10 +89,15 @@ class SellerProductTest extends TestCase
     public function testASellerCanListItsProducts(): void
     {
         $products = Product::factory([
-            "seller_id" => $this->user->id,
+            'seller_id' => $this->user->id,
         ])
             ->count(3)
             ->create();
+        Product::factory([
+            'seller_id' => -1,
+            'name' => 'Fanta',
+            'cost' => 3
+        ])->create();
 
         $request = Request::capture();
         $expectedResponse = SellerProductResource::collection($products)->toArray($request);
@@ -117,5 +131,54 @@ class SellerProductTest extends TestCase
             ->assertJson([
                 'data' => $expectedResponse,
             ]);
+    }
+
+    public function testASellerCanFilterProducts(): void
+    {
+        $product1 = Product::factory([
+            'seller_id' => $this->user->id,
+            'name' => 'Cola',
+            'cost' => 2
+        ])->create();
+        Product::factory([
+            'seller_id' => -1,
+            'name' => 'Fanta',
+            'cost' => 4
+        ])->create();
+        $product3 = Product::factory([
+            'seller_id' => $this->user->id,
+            'name' => 'Beer',
+            'cost' => 5
+        ])->create();
+
+        $request = Request::capture();
+        $transformedProducts = SellerProductResource::collection(
+            [$product1, $product3]
+        )->toArray($request);
+
+        $this
+            ->json('GET', $this->routes['index-filter-by-name'])
+            ->assertStatus(Response::HTTP_OK)
+            ->assertJsonCount(1, 'data')
+            ->assertJson([
+                'data' => [
+                    $transformedProducts[0]
+                ],
+            ]);
+
+        $this
+            ->json('GET', $this->routes['index-filter-by-cost'])
+            ->assertStatus(Response::HTTP_OK)
+            ->assertJsonCount(1, 'data')
+            ->assertJson([
+                'data' => [
+                    $transformedProducts[1]
+                ],
+            ]);
+
+        $this
+            ->json('GET', $this->routes['index-filter-by-seller'])
+            ->assertStatus(Response::HTTP_OK)
+            ->assertJsonCount(0, 'data');
     }
 }
